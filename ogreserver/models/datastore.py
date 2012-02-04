98a3a1c88306a7da1aede55a79e1586f216590d4
mafro <dev@mafro.net>
2012-02-04 08:09:05 +0000

    refactored DataStore class

--- ogreserver/models/datastore.py
+++ ogreserver/models/datastore.py
@@ -3,20 +3,73 @@ from boto.s3.key import Key
 import os, hashlib
 
 
+class Factory():
+
+    sdb = None
+    bookdb = None
+    formatdb = None
+    versiondb = None
+    s3 = None
+    bucket = None
+
+    @staticmethod
+    def connect_sdb():
+        if Factory.sdb is None:
+            Factory.sdb = boto.connect_sdb(app.config['AWS_ACCESS_KEY'], app.config['AWS_SECRET_KEY'])
+
+        return Factory.sdb
+
+    @staticmethod
+    def connect_bookdb():
+        Factory.connect_sdb()
+
+        if Factory.bookdb is None:
+            Factory.bookdb = Factory.sdb.get_domain("ogre_books")
+
+        return Factory.bookdb
+
+    @staticmethod
+    def connect_formatdb():
+        Factory.connect_sdb()
+
+        if Factory.formatdb is None:
+            Factory.formatdb = Factory.sdb.get_domain("ogre_formats")
+
+        return Factory.formatdb
+
+    @staticmethod
+    def connect_versiondb():
+        Factory.connect_sdb()
+
+        if Factory.versiondb is None:
+            Factory.versiondb = Factory.sdb.get_domain("ogre_versions")
+
+        return Factory.versiondb
+
+    @staticmethod
+    def connect_s3():
+        if Factory.s3 is None:
+            Factory.s3 = boto.connect_s3(app.config['AWS_ACCESS_KEY'], app.config['AWS_SECRET_KEY'])
+
+        if Factory.bucket is None:
+            Factory.bucket = Factory.s3.get_bucket(app.config['S3_BUCKET'])
+
+        return Factory.bucket
+
+
 class DataStore():
 
     def __init__(self, user):
-        # create connection to sdb
-        self.sdb = boto.connect_sdb(app.config['AWS_ACCESS_KEY'], app.config['AWS_SECRET_KEY'])
-        self.bookdb = self.sdb.get_domain("ogre_books")
-        self.formatdb = self.sdb.get_domain("ogre_formats")
-        self.versiondb = self.sdb.get_domain("ogre_versions")
         self.user = user
 
     def update_library(self, ebooks):
+        bookdb = Factory.connect_bookdb()
+        formatdb = Factory.connect_formatdb()
+        versiondb = Factory.connect_versiondb()
+
         for authortitle in ebooks.keys():
             # check for this in the library
-            b = self.bookdb.get_item(authortitle)
+            b = bookdb.get_item(authortitle)
 
             if b is None:
                 self.create_book_entry(authortitle, ebooks[authortitle].keys())
@@ -34,7 +87,7 @@ class DataStore():
 
                 # check if supplied formats already exist
                 for fmt in ebooks[authortitle].keys():
-                    f = self.formatdb.get_item(authortitle+fmt)
+                    f = formatdb.get_item(authortitle+fmt)
 
                     # append to the set of this book's formats
                     if fmt not in b['formats']:
@@ -46,8 +99,7 @@ class DataStore():
                         key = self.create_version_entry(authortitle, fmt, 1, ebooks[authortitle][fmt]['size'], ebooks[authortitle][fmt]['filehash'])
                     else:
                         # format exists; ensure this exact version hasn't already been uploaded
-                        rs = self.sdb.select("ogre_versions", "select filehash from ogre_versions where filehash = '%s'" % ebooks[authortitle][fmt]['filehash'])
-                        if len(rs) == 0:
+                        if self.check_version_exists(ebooks[authortitle][fmt]['filehash']):
                             # increment the count of different versions of this format
                             f['version_count'] = int(f['version_count']) + 1
                             f.save()
@@ -59,8 +111,9 @@ class DataStore():
 
 
     def create_book_entry(self, authortitle, formats):
+        bookdb = Factory.connect_bookdb()
         key = hashlib.md5(authortitle).hexdigest()
-        b = self.bookdb.new_item(authortitle)
+        b = bookdb.new_item(authortitle)
         b.add_value("authortitle", authortitle)
         b.add_value("users", self.user.username)
         b.add_value("formats", formats)
@@ -69,8 +122,9 @@ class DataStore():
         return key
 
     def create_format_entry(self, authortitle, fmt):
+        formatdb = Factory.connect_formatdb()
         key = hashlib.md5(authortitle + fmt).hexdigest()
-        f = self.formatdb.new_item(key)
+        f = formatdb.new_item(key)
         f.add_value("authortitle", authortitle)
         f.add_value("format", fmt)
         f.add_value("version_count", 1)
@@ -79,8 +133,9 @@ class DataStore():
         return key
 
     def create_version_entry(self, authortitle, fmt, version, size, filehash):
+        versiondb = Factory.connect_versiondb()
         key = hashlib.md5(authortitle + '-' + str(version) + fmt).hexdigest()
-        v = self.versiondb.new_item(key)
+        v = versiondb.new_item(key)
         v.add_value("authortitle", authortitle)
         v.add_value("format", fmt)
         v.add_value("user", self.user.username)
@@ -90,18 +145,21 @@ class DataStore():
         v.save()
         return key
 
+    def check_version_exists(self, filehash):
+        sdb = Factory.connect_sdb()
+        rs = sdb.select("ogre_versions", "select filehash from ogre_versions where filehash = '%s'" % filehash)
+        return (len(rs) > 0)
 
     def set_uploaded(self, sdbkey):
-        v = self.versiondb.get_item(sdbkey)
+        versiondb = Factory.connect_versiondb()
+        v = versiondb.get_item(sdbkey)
         v.add_value("uploaded", True)
         v.save()
 
-
     def store_ebook(self, sdbkey, filehash, filepath):
         # connect to S3
-        self.s3 = boto.connect_s3(app.config['AWS_ACCESS_KEY'], app.config['AWS_SECRET_KEY'])
-        b = self.s3.get_bucket(app.config['S3_BUCKET'])
-        k = Key(b)
+        bucket = Factory.connect_s3()
+        k = Key(bucket)
         k.key = sdbkey
 
         # calculate uploaded file md5
@@ -123,7 +181,6 @@ class DataStore():
         # always delete local file
         os.remove(filepath)
 
-
     def verify_ebook(self, sdbkey):
         # TODO @periodic verify
         # load SDB record
@@ -133,13 +190,14 @@ class DataStore():
         # delete file
         pass
 
-
     def find_missing_books(self):
         # query for books which have no uploaded file
-        return self.sdb.select("ogre_versions", "select sdbkey, filehash from ogre_versions where uploaded is null and user = '%s'" % self.user.username)
-
+        sdb = Factory.connect_sdb()
+        return sdb.select("ogre_versions", "select sdbkey, filehash from ogre_versions where uploaded is null and user = '%s'" % self.user.username)
 
     def update_timestamp(self):
+        bookdb = Factory.connect_bookdb()
+
         # set library updated timestamp
         meta = bookdb.get_item("meta")
         if meta == None:

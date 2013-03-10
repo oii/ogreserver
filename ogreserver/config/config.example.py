1dd3f36fcf0f7f3d053d969c7f52d636e6045d00
mafro <github@mafro.net>
2013-03-11 10:41:19 +1100

    updated example config file

--- ogreserver/config/config.example.py
+++ ogreserver/config/config.example.py
@@ -1,17 +1,26 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # AWS config
 AWS_ACCESS_KEY = ""
 AWS_SECRET_KEY = ""
 S3_BUCKET = ""
 
 # Flask app
-DATABASE = ""
 DEBUG = True
-SECRET_KEY = ""
-USERNAME = ""
-PASSWORD = ""
+BETA = True
 
-# DB
+# Flask-WTF
+SECRET_KEY = ""
+
+# SQLAlchemy DB URI
 SQLALCHEMY_DATABASE_URI = ""
+
+# Whoosh full-text search
+WHOOSH_BASE = os.path.join(basedir, "search.db")
 
 # Upload path
 UPLOADED_EBOOKS_DEST = ""
+
+# Ebook conversion formats; all books will be provided in these formats by OGRE
+EBOOK_FORMATS = ['epub', 'mobi']

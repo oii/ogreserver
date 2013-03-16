a01293168b4755067cbb4fa9a38162fdba660eef
mafro <github@mafro.net>
2013-03-16 15:45:28 +1100

    handler for custom error pages

--- ogreserver/__init__.py
+++ ogreserver/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 # import Flask library
-from flask import Flask
+from flask import Flask, render_template
 
 # import Flask extensions
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -11,6 +11,16 @@ from flask.ext.uploads import UploadSet, ALL, configure_uploads
 # instantiate Flask application
 app = Flask(__name__)
 app.config.from_pyfile("config/flask.app.conf.py")
+
+# declare some error handler pages
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template("500.html"), 500
 
 
 # setup SQLAlchemy
@@ -44,6 +54,7 @@ def load_user(userid):
     from ogreserver.models.user import User
     user = User.query.filter_by(id=int(userid)).first()
     return user
+
 
 # setup Flask-Upload
 uploads = UploadSet('ebooks', ALL)

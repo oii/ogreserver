b7b89bf0ecb986850b7ee1f28e0362a58a325c6b
mafro <github@mafro.net>
2014-07-02 09:01:44 +1000

    tests: removed test_duplicate in integration; implemented elsewhere

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,35 +5,37 @@ import pytest
 
 # import ogreclient installed locally
 from ogreclient.cli import download_dedrm
-from ogreclient.core import authenticate, sync_with_server
+#from ogreclient.core import authenticate, sync_with_server
 from ogreclient.printer import DummyPrinter
 
 
+@pytest.mark.xfail
 def test_duplicate(ogreserver, client_config, tmpdir):
+    pass
     # setup ebook home for this test
-    client_config['ebook_home'] = tmpdir.strpath
+    #client_config['ebook_home'] = tmpdir.strpath
 
-    prntr = DummyPrinter()
+    #prntr = DummyPrinter()
 
-    # setup a sync data object
-    data = {
-        "Carroll, Lewis - Alice's Adventures in Wonderland": {
-            'format': 'epub',
-            'file_hash': '42344f0e247923fcb347c0e5de5fc762',
-            'owner': 'test',
-            'size': 69339,
-        }
-    }
+    ## setup a sync data object
+    #data = {
+    #    "Carroll, Lewis - Alice's Adventures in Wonderland": {
+    #        'format': 'epub',
+    #        'file_hash': '42344f0e247923fcb347c0e5de5fc762',
+    #        'owner': 'test',
+    #        'size': 69339,
+    #    }
+    #}
 
-    session_key = authenticate(client_config['host'], client_config['username'], client_config['password'])
-    response = sync_with_server(client_config, prntr, session_key, data)
+    #session_key = authenticate(client_config['host'], client_config['username'], client_config['password'])
+    #response = sync_with_server(client_config, prntr, session_key, data)
 
-    assert len(response['ebooks_to_upload']) == 1
-    assert response['ebooks_to_update']['42344f0e247923fcb347c0e5de5fc762']['dupe'] is False
+    #assert len(response['ebooks_to_upload']) == 1
+    #assert response['ebooks_to_update']['42344f0e247923fcb347c0e5de5fc762']['dupe'] is False
 
-    # 2) sync the same book and ensure duplicate registered
-    response = sync_with_server(client_config, prntr, session_key, data)
-    assert response['ebooks_to_update']['42344f0e247923fcb347c0e5de5fc762']['dupe'] is True
+    ## 2) sync the same book and ensure duplicate registered
+    #response = sync_with_server(client_config, prntr, session_key, data)
+    #assert response['ebooks_to_update']['42344f0e247923fcb347c0e5de5fc762']['dupe'] is True
 
 
 @pytest.mark.xfail

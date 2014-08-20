ab57027c8402d1b47fa8e4c96f5a010dd3a70b07
mafro <github@mafro.net>
2014-08-21 08:57:10 +1000

    ui: upgrade to foundation 5

--- ogreserver/static/config.rb
+++ ogreserver/static/config.rb
@@ -1,13 +1,16 @@
-require 'zurb-foundation'
-# Require any additional compass plugins here.
+require 'compass'
 
+# Require any additional compass plugins here.
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
+
+Encoding.default_external = "utf-8"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed

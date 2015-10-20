e0a283acec0d87a88a38f3deb0fff3d270eed1d5
Matt Black <github@mafro.net>
2015-10-20 18:50:40 +0100

    server: mocked out Amazon API calls with fixtures

--- ogreserver/tests/conftest.py
+++ ogreserver/tests/conftest.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 import boto.exception
 import mock
+import os
 import pytest
+import yaml
 
 from ..utils import connect_s3
 from ..models.amazon import AmazonAPI
@@ -23,7 +25,7 @@ def s3bucket(app_config):
     s3.delete_bucket(bucket)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def amazon(app_config, logger):
     return AmazonAPI(
         logger,
@@ -31,6 +33,12 @@ def amazon(app_config, logger):
         app_config.get('AWS_ADVERTISING_API_SECRET_KEY', None),
         app_config.get('AWS_ADVERTISING_API_ASSOCIATE_TAG', None),
     )
+
+@pytest.yield_fixture(scope='function')
+def mock_amazon():
+    m = mock.patch('ogre.ogreserver.models.amazon.bottlenose.Amazon')
+    yield m.start()
+    m.stop()
 
 
 @pytest.fixture(scope='session')
@@ -64,3 +72,21 @@ def mock_compute_md5(request):
     m = mock.patch('ogre.ogreserver.models.conversion.compute_md5')
     yield m.start()
     m.stop()
+
+
+@pytest.fixture(scope='session')
+def get_data_fixtures():
+    """
+    Load a set data fixtures for a particular test. Fixtures must be stored in:
+        <test_filename>_fixtures/<testname>.yaml
+
+    This YAML file should contain all fixtures for the test.
+    """
+    def get_data_fixture_wrapped(file_path, test_name):
+        fixture_path = os.path.join(
+            '{}_fixtures'.format(file_path[:-3]),
+            '{}.yaml'.format(test_name.split('.')[-1:][0])
+        )
+        with open(fixture_path, 'r') as f:
+            return yaml.load(f.read())
+    return get_data_fixture_wrapped
--- ogreserver/tests/test_amazon_api.py
+++ ogreserver/tests/test_amazon_api.py
@@ -2,13 +2,23 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 
-def test_author_title_search1(amazon):
+def test_author_title_search1(mock_amazon, amazon, get_data_fixtures):
+    # load AmazonAPI response fixtures
+    fixtures = get_data_fixtures(__file__, 'test_author_title_search1')
+    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
+    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+
     # search author, title on Amazon
     am_data = amazon.search(author='ccc ccc', title='Lee, Harper--To Kill a Mockingbird')
     assert am_data is None
 
 
-def test_author_title_search2(amazon):
+def test_author_title_search2(mock_amazon, amazon, get_data_fixtures):
+    # load AmazonAPI response fixtures
+    fixtures = get_data_fixtures(__file__, 'test_author_title_search2')
+    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
+    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+
     am_data = amazon.search(author='Richard Morgan', title='Black Man')
     assert am_data['asin'] == '2352942322'
     assert am_data['author'] == 'Richard Morgan'
@@ -17,7 +27,12 @@ def test_author_title_search2(amazon):
     assert am_data['url'] == 'http://www.amazon.com/Black-Man-Richard-Morgan/dp/2352942322'
 
 
-def test_author_title_search3(amazon):
+def test_author_title_search3(mock_amazon, amazon, get_data_fixtures):
+    # load AmazonAPI response fixtures
+    fixtures = get_data_fixtures(__file__, 'test_author_title_search3')
+    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
+    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+
     am_data = amazon.search(author='Max Brooks', title='World War Z')
     assert am_data['asin'] == 'B000JMKQX0'
     assert am_data['author'] == 'Max Brooks'
@@ -26,7 +41,12 @@ def test_author_title_search3(amazon):
     assert am_data['url'] == 'http://www.amazon.com/World-War-Oral-History-Zombie-ebook/dp/B000JMKQX0'
 
 
-def test_author_title_search4(amazon):
+def test_author_title_search4(mock_amazon, amazon, get_data_fixtures):
+    # load AmazonAPI response fixtures
+    fixtures = get_data_fixtures(__file__, 'test_author_title_search4')
+    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
+    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+
     am_data = amazon.search(author='Richard Morgan', title='Altered Carbon (GOLLANCZ S.F.)')
     assert am_data['asin'] == 'B000FBFMZ2'
     assert am_data['author'] == 'Richard K. Morgan'
@@ -35,7 +55,12 @@ def test_author_title_search4(amazon):
     assert am_data['url'] == 'http://www.amazon.com/Altered-Carbon-Takeshi-Kovacs-Novels-ebook/dp/B000FBFMZ2'
 
 
-def test_invalid_asin_search(amazon):
+def test_invalid_asin_search(mock_amazon, amazon, get_data_fixtures):
+    # load AmazonAPI response fixtures
+    fixtures = get_data_fixtures(__file__, 'test_invalid_asin_search')
+    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
+    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+
     # search for a Kindle ASIN; which will raise AmazonItemNotAccessibleError
     # and subsequently search by author/title
     am_data = amazon.search(asin='B003WE9TU8', author='Max Brooks', title='World War Z')
@@ -46,7 +71,12 @@ def test_invalid_asin_search(amazon):
     assert am_data['url'] == 'http://www.amazon.com/World-War-Oral-History-Zombie-ebook/dp/B000JMKQX0'
 
 
-def test_valid_asin_search(amazon):
+def test_valid_asin_search(mock_amazon, amazon, get_data_fixtures):
+    # load AmazonAPI response fixtures
+    fixtures = get_data_fixtures(__file__, 'test_valid_asin_search')
+    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
+    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+
     # search for a valid ASIN
     am_data = amazon.search(asin='B000JMKQX0')
     assert am_data['asin'] == 'B000JMKQX0'

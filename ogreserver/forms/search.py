bf3221a725fe76857edcfe1397d361b9d35f95c7
Matt Black <github@mafro.net>
2015-12-04 16:34:03 +0000

    site: advanced search filter form

--- ogreserver/forms/search.py
+++ ogreserver/forms/search.py
@@ -2,8 +2,10 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from flask.ext.wtf import Form
-from wtforms import TextField
+from wtforms import TextField, BooleanField
 
 
 class SearchForm(Form):
     s = TextField('s')
+    is_curated = BooleanField('Curated Only?', default=True)
+    is_fiction = BooleanField('Fiction Only?', default=True)
--- ogreserver/static/js/app.js
+++ ogreserver/static/js/app.js
@@ -101,3 +101,16 @@ if(typeof String.prototype.endsWith !== 'function') {
         return this.indexOf(suffix, this.length - suffix.length) !== -1;
     };
 }
+
+$(document).ready(function() {
+	$('#advanced').click(function() {
+		if($('#advanced-form').hasClass('advanced-open')) {
+			$('#advanced-form').removeClass('advanced-open');
+			$('#advanced-form').addClass('advanced-closed');
+		}else{
+			$('#advanced-form').removeClass('advanced-closed');
+			$('#advanced-form').addClass('advanced-open');
+		}
+		return false;
+	});
+});

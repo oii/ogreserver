e81425d0aa725da10692ed013c12fb11df34b103
Matt Black <github@mafro.net>
2016-04-22 13:12:39 +0100

    server: updated logging format configuration

--- ogreserver/extensions/logging.py
+++ ogreserver/extensions/logging.py
@@ -10,15 +10,19 @@ def init_logging(app):
         # set logging level for production
         app.logger.setLevel(app.config.get('LOGGING_LEVEL', logging.ERROR))
 
-    # setup the log format
-    app.debug_log_format = '%(asctime)s [%(levelname)s] in %(module)s %(message)s'
-    log_date_format = '%d/%m/%Y %H:%M:%S'
-    app.logger.handlers[0].formatter.datefmt = log_date_format
+    # setup the log format on Flask's logger
+    app.logger.handlers[0].setFormatter(
+        logging.Formatter(
+            '%(asctime)s [%(levelname)s] in %(module)s %(message)s', '%d/%m/%Y %H:%M:%S'
+        )
+    )
 
     if 'TESTING' in app.config and app.config['TESTING'] is True:
         # during integration tests log to a file, since Flask is running on a background thread
         handler = logging.FileHandler(app.config['TESTING_LOG_PATH'])
         handler.setFormatter(
-            logging.Formatter(app.debug_log_format, datefmt=log_date_format)
+            logging.Formatter(
+                '%(asctime)s [%(levelname)s] in %(module)s %(message)s', '%d/%m/%Y %H:%M:%S'
+            )
         )
         app.logger.addHandler(handler)

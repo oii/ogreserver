1f9697aebe58ecfe9a2f1abe387fb5e354128ee2
Matt Black <github@mafro.net>
2016-09-06 07:38:19 +0100

    client: updated version info

--- ogreclient/ogreclient/__init__.py
+++ ogreclient/ogreclient/__init__.py
@@ -3,8 +3,8 @@
 #
 
 __title__ = 'ogreclient'
-__version__ = '0.0.1'
-__build__ = 0x000001
-__author__ = 'Oii'
+__version__ = '0.0.2'
+__build__ = 0x000002
+__author__ = 'mafrosis'
 __license__ = 'Simplified BSD License'
-__copyright__ = 'Copyright 2014 Oii'
+__copyright__ = 'Copyright 2012-2016 Oii'

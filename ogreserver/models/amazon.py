2a7aef6eb4d3ec0abfde0c49529608799d6f4c8b
Matt Black <github@mafro.net>
2016-11-09 18:33:34 +0000

    server: XML namespace change for Amazon API

--- ogreserver/models/amazon.py
+++ ogreserver/models/amazon.py
@@ -14,7 +14,7 @@ from ..utils import clean_string
 
 
 class AmazonAPI:
-    NS = 'http://webservices.amazon.com/AWSECommerceService/2011-08-01'
+    NS = 'http://webservices.amazon.com/AWSECommerceService/2013-08-01'
 
     def __init__(self, logger, access_key, secret_key, associate_tag, match_threshold=50):
         self.logger = logger

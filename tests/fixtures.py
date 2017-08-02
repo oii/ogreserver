9f4716ee18cf54667aed9cc1fcf25570cf5ba44d
Matt Black <github@mafro.net>
2017-08-02 11:39:05 +0100

    [tests] refactor AmazonAPI tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -54,3 +54,309 @@ EBOOK_FIXTURE_3 = {
     },
     'size': 746218
 }
+
+AMAZON_TEST_AUTHOR_TITLE_SEARCH_NO_MATCHES_ITEMSEARCH_NOMATCH = """<?xml version="1.0" ?>
+<ItemSearchResponse xmlns="http://webservices.amazon.com/AWSECommerceService/2013-08-01">
+    <OperationRequest>
+        <HTTPHeaders>
+            <Header Name="UserAgent" Value="Python-urllib/2.7" />
+        </HTTPHeaders>
+        <RequestId>025b6728-cf2c-4648-aaf0-852ef927cc53</RequestId>
+        <Arguments>
+            <Argument Name="AWSAccessKeyId" Value="egg" />
+            <Argument Name="AssociateTag" Value="o02a-21" />
+            <Argument Name="Keywords" Value="ccc ccc Lee, Harper--To Kill a Mockingbird" />
+            <Argument Name="Operation" Value="ItemSearch" />
+            <Argument Name="SearchIndex" Value="KindleStore" />
+            <Argument Name="Service" Value="AWSECommerceService" />
+            <Argument Name="Timestamp" Value="2017-08-01T15:53:06Z" />
+            <Argument Name="Version" Value="2013-08-01" />
+            <Argument Name="Signature" Value="ZOcThowp45jkL0GmiXVotkdy0hAQzZfXJVj/DztvneE=" />
+        </Arguments>
+        <RequestProcessingTime>0.0140179890000000</RequestProcessingTime>
+    </OperationRequest>
+    <Items>
+        <Request>
+            <IsValid>True</IsValid>
+            <ItemSearchRequest>
+                <Keywords>ccc ccc Lee, Harper--To Kill a Mockingbird</Keywords>
+                <ResponseGroup>Small</ResponseGroup>
+                <SearchIndex>KindleStore</SearchIndex>
+            </ItemSearchRequest>
+            <Errors>
+                <Error>
+                    <Code>AWS.ECommerceService.NoExactMatches</Code>
+                    <Message>We did not find any matches for your request.</Message>
+                </Error>
+            </Errors>
+        </Request>
+        <TotalResults>0</TotalResults>
+        <TotalPages>0</TotalPages>
+        <MoreSearchResultsUrl>https://www.amazon.co.uk/gp/search?linkCode=xm2&amp;SubscriptionId=egg&amp;keywords=ccc%20ccc%20Lee%2C%20Harper--To%20Kill%20a%20Mockingbird&amp;tag=o02a-21&amp;creative=12734&amp;url=search-alias%3Ddigital-text&amp;camp=2025</MoreSearchResultsUrl>
+    </Items>
+</ItemSearchResponse>
+"""
+
+AMAZON_TEST_AUTHOR_TITLE_SEARCH_NO_MATCHES_ITEMSEARCH_RESULT = """<?xml version="1.0" ?>
+<ItemSearchResponse xmlns="http://webservices.amazon.com/AWSECommerceService/2013-08-01">
+    <OperationRequest>
+        <HTTPHeaders>
+            <Header Name="UserAgent" Value="Python-urllib/2.7" />
+        </HTTPHeaders>
+        <RequestId>cd0d696c-2e9f-4a0f-ad82-6bdf4ea4ea4d</RequestId>
+        <Arguments>
+            <Argument Name="AWSAccessKeyId" Value="egg" />
+            <Argument Name="AssociateTag" Value="o02a-21" />
+            <Argument Name="Keywords" Value="ccc ccc Lee, Harper--To Kill a Mockingbird" />
+            <Argument Name="Operation" Value="ItemSearch" />
+            <Argument Name="SearchIndex" Value="Books" />
+            <Argument Name="Service" Value="AWSECommerceService" />
+            <Argument Name="Timestamp" Value="2017-08-01T15:53:35Z" />
+            <Argument Name="Version" Value="2013-08-01" />
+            <Argument Name="Signature" Value="ME7bg8w/xzn74REy6+hSdhX2Et9HKUyUitsQJ0hSjZg=" />
+        </Arguments>
+        <RequestProcessingTime>0.0278894580000000</RequestProcessingTime>
+    </OperationRequest>
+    <Items>
+        <Request>
+            <IsValid>True</IsValid>
+            <ItemSearchRequest>
+                <Keywords>ccc ccc Lee, Harper--To Kill a Mockingbird</Keywords>
+                <ResponseGroup>Small</ResponseGroup>
+                <SearchIndex>Books</SearchIndex>
+            </ItemSearchRequest>
+            <Errors>
+                <Error>
+                    <Code>AWS.ECommerceService.NoExactMatches</Code>
+                    <Message>We did not find any matches for your request.</Message>
+                </Error>
+            </Errors>
+        </Request>
+        <TotalResults>0</TotalResults>
+        <TotalPages>0</TotalPages>
+        <MoreSearchResultsUrl>https://www.amazon.co.uk/gp/search?linkCode=xm2&amp;SubscriptionId=egg&amp;keywords=ccc%20ccc%20Lee%2C%20Harper--To%20Kill%20a%20Mockingbird&amp;tag=o02a-21&amp;creative=12734&amp;url=search-alias%3Dbooks-single-index&amp;camp=2025</MoreSearchResultsUrl>
+    </Items>
+</ItemSearchResponse>
+"""
+
+AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMSEARCH = """<?xml version="1.0" ?>
+<ItemSearchResponse xmlns="http://webservices.amazon.com/AWSECommerceService/2013-08-01">
+    <OperationRequest>
+        <HTTPHeaders>
+            <Header Name="UserAgent" Value="Python-urllib/2.7" />
+        </HTTPHeaders>
+        <RequestId>6fad145d-dd7a-4bcd-97e7-9fd020f3299b</RequestId>
+        <Arguments>
+            <Argument Name="AWSAccessKeyId" Value="egg" />
+            <Argument Name="AssociateTag" Value="o02a-21" />
+            <Argument Name="Keywords" Value="Richard Morgan Black Man" />
+            <Argument Name="Operation" Value="ItemSearch" />
+            <Argument Name="SearchIndex" Value="KindleStore" />
+            <Argument Name="Service" Value="AWSECommerceService" />
+            <Argument Name="Timestamp" Value="2017-08-02T06:25:14Z" />
+            <Argument Name="Version" Value="2013-08-01" />
+            <Argument Name="Signature" Value="MefQx5fchD4G5mpSakKQuUvVZcq2YJipUob4jv5qFqw=" />
+        </Arguments>
+        <RequestProcessingTime>0.0240643290000000</RequestProcessingTime>
+    </OperationRequest>
+    <Items>
+        <Request>
+            <IsValid>True</IsValid>
+            <ItemSearchRequest>
+                <Keywords>Richard Morgan Black Man</Keywords>
+                <ResponseGroup>Small</ResponseGroup>
+                <SearchIndex>KindleStore</SearchIndex>
+            </ItemSearchRequest>
+        </Request>
+        <TotalResults>2</TotalResults>
+        <TotalPages>1</TotalPages>
+        <MoreSearchResultsUrl>https://www.amazon.co.uk/gp/search?linkCode=xm2&amp;SubscriptionId=egg&amp;keywords=Richard%20Morgan%20Black%20Man&amp;tag=o02a-21&amp;creative=12734&amp;url=search-alias%3Ddigital-text&amp;camp=2025</MoreSearchResultsUrl>
+        <Item>
+            <ASIN>B002U3CBZ2</ASIN>
+            <DetailPageURL>https://www.amazon.co.uk/Black-GOLLANCZ-S-F-Richard-Morgan-ebook/dp/B002U3CBZ2?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=165953&amp;creativeASIN=B002U3CBZ2</DetailPageURL>
+            <ItemLinks>
+                <ItemLink>
+                    <Description>Add To Wishlist</Description>
+                    <URL>https://www.amazon.co.uk/gp/registry/wishlist/add-item.html?asin.0=B002U3CBZ2&amp;SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B002U3CBZ2</URL>
+                </ItemLink>
+                <ItemLink>
+                    <Description>Tell A Friend</Description>
+                    <URL>https://www.amazon.co.uk/gp/pdp/taf/B002U3CBZ2?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B002U3CBZ2</URL>
+                </ItemLink>
+                <ItemLink>
+                    <Description>All Customer Reviews</Description>
+                    <URL>https://www.amazon.co.uk/review/product/B002U3CBZ2?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B002U3CBZ2</URL>
+                </ItemLink>
+                <ItemLink>
+                    <Description>All Offers</Description>
+                    <URL>https://www.amazon.co.uk/gp/offer-listing/B002U3CBZ2?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B002U3CBZ2</URL>
+                </ItemLink>
+            </ItemLinks>
+            <ItemAttributes>
+                <Author>Richard Morgan</Author>
+                <Manufacturer>Gollancz</Manufacturer>
+                <ProductGroup>eBooks</ProductGroup>
+                <Title>Black Man (GOLLANCZ S.F.)</Title>
+            </ItemAttributes>
+        </Item>
+        <Item>
+            <ASIN>B005PR0Y4A</ASIN>
+            <DetailPageURL>https://www.amazon.co.uk/Black-SCIENCE-FICTION-French-Richard-Morgan-ebook/dp/B005PR0Y4A?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=165953&amp;creativeASIN=B005PR0Y4A</DetailPageURL>
+            <ItemLinks>
+                <ItemLink>
+                    <Description>Add To Wishlist</Description>
+                    <URL>https://www.amazon.co.uk/gp/registry/wishlist/add-item.html?asin.0=B005PR0Y4A&amp;SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B005PR0Y4A</URL>
+                </ItemLink>
+                <ItemLink>
+                    <Description>Tell A Friend</Description>
+                    <URL>https://www.amazon.co.uk/gp/pdp/taf/B005PR0Y4A?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B005PR0Y4A</URL>
+                </ItemLink>
+                <ItemLink>
+                    <Description>All Customer Reviews</Description>
+                    <URL>https://www.amazon.co.uk/review/product/B005PR0Y4A?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B005PR0Y4A</URL>
+                </ItemLink>
+                <ItemLink>
+                    <Description>All Offers</Description>
+                    <URL>https://www.amazon.co.uk/gp/offer-listing/B005PR0Y4A?SubscriptionId=egg&amp;tag=o02a-21&amp;linkCode=xm2&amp;camp=2025&amp;creative=12734&amp;creativeASIN=B005PR0Y4A</URL>
+                </ItemLink>
+            </ItemLinks>
+            <ItemAttributes>
+                <Author>Richard Morgan</Author>
+                <Manufacturer>Milady</Manufacturer>
+                <ProductGroup>eBooks</ProductGroup>
+                <Title>Black Man (SCIENCE-FICTION) (French Edition)</Title>
+            </ItemAttributes>
+        </Item>
+    </Items>
+</ItemSearchResponse>
+"""
+
+AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMLOOKUP = """<?xml version="1.0" ?>
+<ItemLookupResponse xmlns="http://webservices.amazon.com/AWSECommerceService/2013-08-01">
+    <OperationRequest>
+        <HTTPHeaders>
+            <Header Name="UserAgent" Value="Python-urllib/2.7" />
+        </HTTPHeaders>
+        <RequestId>65d55c6c-19f7-4dc1-b6cb-578f2583f525</RequestId>
+        <Arguments>
+            <Argument Name="AWSAccessKeyId" Value="egg" />
+            <Argument Name="AssociateTag" Value="o02a-21" />
+            <Argument Name="ItemId" Value="B002U3CBZ2" />
+            <Argument Name="Operation" Value="ItemLookup" />
+            <Argument Name="ResponseGroup" Value="Images" />
+            <Argument Name="Service" Value="AWSECommerceService" />
+            <Argument Name="Timestamp" Value="2017-08-02T06:29:46Z" />
+            <Argument Name="Version" Value="2013-08-01" />
+            <Argument Name="Signature" Value="0geD4slhwt8YTRCvyO8D90Mi9Yxj16L5JoHCI8zwXNM=" />
+        </Arguments>
+        <RequestProcessingTime>0.0038972700000000</RequestProcessingTime>
+    </OperationRequest>
+    <Items>
+        <Request>
+            <IsValid>True</IsValid>
+            <ItemLookupRequest>
+                <IdType>ASIN</IdType>
+                <ItemId>B002U3CBZ2</ItemId>
+                <ResponseGroup>Images</ResponseGroup>
+                <VariationPage>All</VariationPage>
+            </ItemLookupRequest>
+        </Request>
+        <Item>
+            <ASIN>B002U3CBZ2</ASIN>
+            <SmallImage>
+                <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL._SL75_.jpg</URL>
+                <Height Units="pixels">75</Height>
+                <Width Units="pixels">49</Width>
+            </SmallImage>
+            <MediumImage>
+                <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL._SL160_.jpg</URL>
+                <Height Units="pixels">160</Height>
+                <Width Units="pixels">104</Width>
+            </MediumImage>
+            <LargeImage>
+                <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL.jpg</URL>
+                <Height Units="pixels">500</Height>
+                <Width Units="pixels">325</Width>
+            </LargeImage>
+            <ImageSets>
+                <ImageSet Category="primary">
+                    <SwatchImage>
+                        <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL._SL30_.jpg</URL>
+                        <Height Units="pixels">30</Height>
+                        <Width Units="pixels">20</Width>
+                    </SwatchImage>
+                    <SmallImage>
+                        <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL._SL75_.jpg</URL>
+                        <Height Units="pixels">75</Height>
+                        <Width Units="pixels">49</Width>
+                    </SmallImage>
+                    <ThumbnailImage>
+                        <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL._SL75_.jpg</URL>
+                        <Height Units="pixels">75</Height>
+                        <Width Units="pixels">49</Width>
+                    </ThumbnailImage>
+                    <TinyImage>
+                        <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL._SL110_.jpg</URL>
+                        <Height Units="pixels">110</Height>
+                        <Width Units="pixels">72</Width>
+                    </TinyImage>
+                    <MediumImage>
+                        <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL._SL160_.jpg</URL>
+                        <Height Units="pixels">160</Height>
+                        <Width Units="pixels">104</Width>
+                    </MediumImage>
+                    <LargeImage>
+                        <URL>https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL.jpg</URL>
+                        <Height Units="pixels">500</Height>
+                        <Width Units="pixels">325</Width>
+                    </LargeImage>
+                    <HiResImage>
+                        <URL>https://images-eu.ssl-images-amazon.com/images/I/81qwu-NBfiL.jpg</URL>
+                        <Height Units="pixels">2560</Height>
+                        <Width Units="pixels">1663</Width>
+                    </HiResImage>
+                </ImageSet>
+            </ImageSets>
+        </Item>
+    </Items>
+</ItemLookupResponse>
+"""
+
+AMAZON_TEST_VALID_ASIN_SEARCH_ITEMSEARCH = """<?xml version="1.0" ?>
+<ItemLookupResponse xmlns="http://webservices.amazon.com/AWSECommerceService/2013-08-01">
+    <OperationRequest>
+        <HTTPHeaders>
+            <Header Name="UserAgent" Value="Python-urllib/2.7" />
+        </HTTPHeaders>
+        <RequestId>607ba22a-bc18-496b-a71c-ffd49a422588</RequestId>
+        <Arguments>
+            <Argument Name="AWSAccessKeyId" Value="egg" />
+            <Argument Name="AssociateTag" Value="egg" />
+            <Argument Name="ItemId" Value="B003WE9TU8" />
+            <Argument Name="Operation" Value="ItemLookup" />
+            <Argument Name="Service" Value="AWSECommerceService" />
+            <Argument Name="Timestamp" Value="2015-10-20T16:00:10Z" />
+            <Argument Name="Version" Value="2013-08-01" />
+            <Argument Name="Signature" Value="CKINO9FqjN7j2y4KNovWDs4I3lyEvG2xht4vgDtwbBg=" />
+        </Arguments>
+        <RequestProcessingTime>0.0094890000000000</RequestProcessingTime>
+    </OperationRequest>
+    <Items>
+        <Request>
+            <IsValid>True</IsValid>
+            <ItemLookupRequest>
+                <IdType>ASIN</IdType>
+                <ItemId>B003WE9TU8</ItemId>
+                <ResponseGroup>Small</ResponseGroup>
+                <VariationPage>All</VariationPage>
+            </ItemLookupRequest>
+            <Errors>
+                <Error>
+                    <Code>AWS.ECommerceService.ItemNotAccessible</Code>
+                    <Message>This item is not accessible through the Product Advertising API.</Message>
+                </Error>
+            </Errors>
+        </Request>
+    </Items>
+</ItemLookupResponse>
+"""
--- tests/sources/test_amazon_api.py
+++ tests/sources/test_amazon_api.py
@@ -3,119 +3,108 @@ from __future__ import unicode_literals
 
 import mock
 
+from ogreserver import exceptions
 from ogreserver.sources.amazon import AmazonAPI
+
+import fixtures
 
 
 @mock.patch('ogreserver.sources.amazon.bottlenose.Amazon')
-def test_author_title_search1(mock_amazon, get_data_fixtures):
+def test_author_title_search_no_matches(mock_amazon):
+    """
+    Validate AmazonAPI class runs three searches before failing on a dodgy author/title
+    """
     amazon = AmazonAPI(None, None, None)
 
-    # load AmazonAPI response fixtures
-    fixtures = get_data_fixtures(__file__, 'test_author_title_search1')
-    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
-    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+    # mock AmazonAPI responses
+    mock_amazon.return_value.ItemSearch.side_effect = [
+        fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_NO_MATCHES_ITEMSEARCH_NOMATCH,
+        fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_NO_MATCHES_ITEMSEARCH_NOMATCH,
+        fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_NO_MATCHES_ITEMSEARCH_RESULT,
+    ]
 
     # search author, title on Amazon
     am_data = amazon.search(author='ccc ccc', title='Lee, Harper--To Kill a Mockingbird')
     assert am_data is None
+    assert mock_amazon.return_value.ItemSearch.call_count == 3
 
 
 @mock.patch('ogreserver.sources.amazon.bottlenose.Amazon')
-def test_author_title_search2(mock_amazon, get_data_fixtures):
+def test_author_title_search_success(mock_amazon):
+    """
+    Validate AmazonAPI finds author/title first time
+    """
     amazon = AmazonAPI(None, None, None)
 
-    # load AmazonAPI response fixtures
-    fixtures = get_data_fixtures(__file__, 'test_author_title_search2')
-    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
-    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+    # mock AmazonAPI responses
+    mock_amazon.return_value.ItemSearch.return_value = fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMSEARCH
+    mock_amazon.return_value.ItemLookup.return_value = fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMLOOKUP
 
     am_data = amazon.search(author='Richard Morgan', title='Black Man')
-    assert am_data['asin'] == '2352942322'
+    assert mock_amazon.return_value.ItemSearch.call_count == 1
+    assert am_data['asin'] == 'B002U3CBZ2'
     assert am_data['author'] == 'Richard Morgan'
-    assert am_data['title'] == 'Black Man'
-    assert am_data['image_url'] == 'http://ecx.images-amazon.com/images/I/51URUN95YpL.jpg'
-    assert am_data['url'] == 'http://www.amazon.com/Black-Man-Richard-Morgan/dp/2352942322'
+    assert am_data['title'] == 'Black Man (GOLLANCZ S.F.)'
+    assert am_data['image_url'] == 'https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL.jpg'
+    assert am_data['url'] == 'https://www.amazon.co.uk/Black-GOLLANCZ-S-F-Richard-Morgan-ebook/dp/B002U3CBZ2?SubscriptionId=egg&tag=o02a-21&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B002U3CBZ2'
 
 
 @mock.patch('ogreserver.sources.amazon.bottlenose.Amazon')
-def test_author_title_search3(mock_amazon, get_data_fixtures):
+def test_invalid_asin_search(mock_amazon):
+    """
+    Search for a Kindle ASIN, which will raise AmazonItemNotAccessibleError
+    Verify a subsequent search for author/title
+    """
     amazon = AmazonAPI(None, None, None)
 
-    # load AmazonAPI response fixtures
-    fixtures = get_data_fixtures(__file__, 'test_author_title_search3')
-    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
-    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+    # mock AmazonAPI responses
+    mock_amazon.return_value.ItemLookup.side_effect = [
+        exceptions.AmazonItemNotAccessibleError,
+        fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMLOOKUP
+    ]
+    mock_amazon.return_value.ItemSearch.return_value = fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMSEARCH
 
-    am_data = amazon.search(author='Max Brooks', title='World War Z')
-    assert am_data['asin'] == 'B000JMKQX0'
-    assert am_data['author'] == 'Max Brooks'
-    assert am_data['title'] == 'World War Z: An Oral History of the Zombie War'
-    assert am_data['image_url'] == 'http://ecx.images-amazon.com/images/I/51ELXqAe9UL.jpg'
-    assert am_data['url'] == 'http://www.amazon.com/World-War-Oral-History-Zombie-ebook/dp/B000JMKQX0'
+    am_data = amazon.search(asin='B003WE9TU8', author='Richard Morgan', title='Black Man')
+    assert mock_amazon.return_value.ItemLookup.call_count == 2
+    assert am_data['asin'] == 'B002U3CBZ2'
+    assert am_data['author'] == 'Richard Morgan'
+    assert am_data['title'] == 'Black Man (GOLLANCZ S.F.)'
+    assert am_data['image_url'] == 'https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL.jpg'
+    assert am_data['url'] == 'https://www.amazon.co.uk/Black-GOLLANCZ-S-F-Richard-Morgan-ebook/dp/B002U3CBZ2?SubscriptionId=egg&tag=o02a-21&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B002U3CBZ2'
 
 
 @mock.patch('ogreserver.sources.amazon.bottlenose.Amazon')
-def test_author_title_search4(mock_amazon, get_data_fixtures):
+def test_valid_asin_search(mock_amazon):
+    """
+    Search for a valid ASIN
+    """
     amazon = AmazonAPI(None, None, None)
 
-    # load AmazonAPI response fixtures
-    fixtures = get_data_fixtures(__file__, 'test_author_title_search4')
-    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
-    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+    # mock AmazonAPI responses
+    mock_amazon.return_value.ItemLookup.side_effect = [
+        fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMSEARCH,
+        fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMLOOKUP
+    ]
 
-    am_data = amazon.search(author='Richard Morgan', title='Altered Carbon (GOLLANCZ S.F.)')
-    assert am_data['asin'] == 'B000FBFMZ2'
-    assert am_data['author'] == 'Richard K. Morgan'
-    assert am_data['title'] == 'Altered Carbon (Takeshi Kovacs Novels Book 1)'
-    assert am_data['image_url'] == 'http://ecx.images-amazon.com/images/I/51hFNetqu5L.jpg'
-    assert am_data['url'] == 'http://www.amazon.com/Altered-Carbon-Takeshi-Kovacs-Novels-ebook/dp/B000FBFMZ2'
+    am_data = amazon.search(asin='B002U3CBZ2', author='Richard Morgan', title='Black Man')
+    assert mock_amazon.return_value.ItemLookup.call_count == 2
+    assert am_data['asin'] == 'B002U3CBZ2'
+    assert am_data['author'] == 'Richard Morgan'
+    assert am_data['title'] == 'Black Man (GOLLANCZ S.F.)'
+    assert am_data['image_url'] == 'https://images-eu.ssl-images-amazon.com/images/I/51fy9mqeVTL.jpg'
+    assert am_data['url'] == 'https://www.amazon.co.uk/Black-GOLLANCZ-S-F-Richard-Morgan-ebook/dp/B002U3CBZ2?SubscriptionId=egg&tag=o02a-21&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B002U3CBZ2'
 
 
 @mock.patch('ogreserver.sources.amazon.bottlenose.Amazon')
-def test_invalid_asin_search(mock_amazon, get_data_fixtures):
+def test_author_title_unicode(mock_amazon):
+    """
+    Ensure the results from AmazonAPI are unicode type
+    """
     amazon = AmazonAPI(None, None, None)
 
-    # load AmazonAPI response fixtures
-    fixtures = get_data_fixtures(__file__, 'test_invalid_asin_search')
-    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
-    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
-
-    # search for a Kindle ASIN; which will raise AmazonItemNotAccessibleError
-    # and subsequently search by author/title
-    am_data = amazon.search(asin='B003WE9TU8', author='Max Brooks', title='World War Z')
-    assert am_data['asin'] == 'B000JMKQX0'
-    assert am_data['author'] == 'Max Brooks'
-    assert am_data['title'] == 'World War Z: An Oral History of the Zombie War'
-    assert am_data['image_url'] == 'http://ecx.images-amazon.com/images/I/51ELXqAe9UL.jpg'
-    assert am_data['url'] == 'http://www.amazon.com/World-War-Oral-History-Zombie-ebook/dp/B000JMKQX0'
-
-
-@mock.patch('ogreserver.sources.amazon.bottlenose.Amazon')
-def test_valid_asin_search(mock_amazon, get_data_fixtures):
-    amazon = AmazonAPI(None, None, None)
-
-    # load AmazonAPI response fixtures
-    fixtures = get_data_fixtures(__file__, 'test_valid_asin_search')
-    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
-    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
-
-    # search for a valid ASIN
-    am_data = amazon.search(asin='B000JMKQX0')
-    assert am_data['asin'] == 'B000JMKQX0'
-    assert am_data['author'] == 'Max Brooks'
-    assert am_data['title'] == 'World War Z: An Oral History of the Zombie War'
-    assert am_data['image_url'] == 'http://ecx.images-amazon.com/images/I/51ELXqAe9UL.jpg'
-    assert am_data['url'] == 'http://www.amazon.com/World-War-Oral-History-Zombie-ebook/dp/B000JMKQX0'
-
-
-@mock.patch('ogreserver.sources.amazon.bottlenose.Amazon')
-def test_author_title_unicode(mock_amazon, get_data_fixtures):
-    amazon = AmazonAPI(None, None, None)
-
-    # load AmazonAPI response fixtures
-    fixtures = get_data_fixtures(__file__, 'test_author_title_search2')
-    mock_amazon.return_value.ItemSearch.side_effect = fixtures['mock_amazon_itemsearch']
-    mock_amazon.return_value.ItemLookup.side_effect = fixtures['mock_amazon_itemlookup']
+    # mock AmazonAPI responses
+    mock_amazon.return_value.ItemSearch.return_value = fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMSEARCH
+    mock_amazon.return_value.ItemLookup.return_value = fixtures.AMAZON_TEST_AUTHOR_TITLE_SEARCH_SUCCESS_ITEMLOOKUP
 
     am_data = amazon.search(author='Richard Morgan', title='Black Man')
     assert type(am_data['asin']) is unicode
